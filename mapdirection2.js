a450c2f97a54f2394dea81f49d55b8f2ea425ae3
miryalguda <137521245+miryalguda@users.noreply.github.com>
2023-09-10 17:52:55 -0400

    Update mapdirection2.js

--- mapdirection2.js
+++ mapdirection2.js
@@ -2,7 +2,7 @@
 mapboxgl.accessToken = '';
 const map = new mapboxgl.Map({
 container: 'map',
-// Choose from Mapbox's core styles, or make your own style with Mapbox Studio
+
 style: 'mapbox://styles/mapbox/streets-v12',
 center: [-79.4512, 43.6568],
 zoom: 13
